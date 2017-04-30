d3d28c585f985f73cbac67b0f2e107def208a5f0
Oscar D. Corbalan <oscardc@gmx.com>
2017-04-30 11:01:46 +0200

    refactor: Use ES6 arrow functions

--- lazyload.js
+++ lazyload.js
@@ -11,100 +11,99 @@ global.inViewport = inViewport;
 replaceGetAttribute('Image');
 replaceGetAttribute('IFrame');
 
-function registerLazyAttr(attr) {
-  if (indexOf.call(lazyAttrs, attr) === -1) {
-    lazyAttrs.push(attr);
-  }
+const registerLazyAttr = attr => {
+    if (indexOf.call(lazyAttrs, attr) === -1) {
+        lazyAttrs.push(attr);
+    }
 }
 
 function lazyload(opts) {
-  opts = merge({
-    'offset': 333,
-    'src': 'data-src',
-    'container': false
-  }, opts || {});
+    opts = merge({
+        'offset': 333,
+        'src': 'data-src',
+        'container': false
+    }, opts || {});
 
-  if (typeof opts.src === 'string') {
-    registerLazyAttr(opts.src);
-  }
-
-  var elts = [];
-
-  function show(elt) {
-    var src = findRealSrc(elt);
-
-    if (src) {
-      elt.src = src;
+    if (typeof opts.src === 'string') {
+        registerLazyAttr(opts.src);
     }
 
-    elt.setAttribute('data-lzled', true);
-    elts[indexOf.call(elts, elt)] = null;
-  }
+    var elts = [];
 
-  function findRealSrc(elt) {
-    if (typeof opts.src === 'function') {
-      return opts.src(elt);
+    const show = elt => {
+        var src = findRealSrc(elt);
+
+        if(src) {
+            elt.src = src;
+        }
+
+        elt.setAttribute('data-lzled', true);
+        elts[indexOf.call(elts, elt)] = null;
     }
 
-    return elt.getAttribute(opts.src);
-  }
+    const findRealSrc = elt => {
+        if (typeof opts.src === 'function') {
+            return opts.src(elt);
+        }
 
-  function register(elt) {
-    // unsubscribe onload
-    // needed by IE < 9, otherwise we get another onload when changing the src
-    elt.onload = null;
-    elt.removeAttribute('onload');
-
-    // https://github.com/vvo/lazyload/issues/62
-    elt.onerror = null;
-    elt.removeAttribute('onerror');
-
-    if (indexOf.call(elts, elt) === -1) {
-      inViewport(elt, opts, show);
+        return elt.getAttribute(opts.src);
     }
-  }
 
-  return register;
+    const register = elt => {
+        // unsubscribe onload
+        // needed by IE < 9, otherwise we get another onload when changing the src
+        elt.onload = null;
+        elt.removeAttribute('onload');
+
+        elt.onerror = null;
+        elt.removeAttribute('onerror');
+
+        if (indexOf.call(elts, elt) === -1) {
+            inViewport(elt, opts, show);
+        }
+    }
+
+    return register;
 }
 
 function replaceGetAttribute(elementName) {
-  var fullname = 'HTML' + elementName + 'Element';
-  if (fullname in global === false) {
-    return;
-  }
-
-  var original = global[fullname].prototype.getAttribute;
-  global[fullname].prototype.getAttribute = function(name) {
-    if (name === 'src') {
-      var realSrc;
-      for (var i = 0, max = lazyAttrs.length; i < max; i++) {
-        realSrc = original.call(this, lazyAttrs[i]);
-        if (realSrc) {
-          break;
-        }
-      }
-
-      return realSrc || original.call(this, name);
+    var fullname = 'HTML' + elementName + 'Element';
+    if (fullname in global === false) {
+        return;
     }
 
-    // our own lazyloader will go through theses lines
-    // because we use getAttribute(opts.src)
-    return original.call(this, name);
-  };
+    var original = global[fullname].prototype.getAttribute;
+    global[fullname].prototype.getAttribute = function(name) {
+        if (name === 'src') {
+            var realSrc;
+            for (var i = 0, max = lazyAttrs.length; i < max; i++) {
+                realSrc = original.call(this, lazyAttrs[i]);
+                if (realSrc) {
+                    break;
+                }
+            }
+
+            return realSrc || original.call(this, name);
+        }
+
+        // our own lazyloader will go through theses lines
+        // because we use getAttribute(opts.src)
+        return original.call(this, name);
+    };
 }
 
-function merge(defaults, opts) {
-  for (var name in defaults) {
-    if (opts[name] === undefined) {
-      opts[name] = defaults[name];
+const merge = (defaults, opts) {
+    for (var name in defaults) {
+        if (opts[name] === undefined) {
+            opts[name] = defaults[name];
+        }
     }
-  }
 
-  return opts;
+    return opts;
 }
 
 // http://webreflection.blogspot.fr/2011/06/partial-polyfills.html
-function indexOf(value) {
-  for (var i = this.length; i-- && this[i] !== value;) {}
-  return i;
+const indexOf = value => {
+    for (var i = this.length; i-- && this[i] !== value;) {}
+    return i;
 }
